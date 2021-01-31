20ea89c162c4d54a84633664b20264ec886411c0
cricketbackground <vadzz.java@gmail.com>
2021-01-31 12:08:43 -0500

    Added uuid generator. Updated App.js to add uuid() to template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import uuid from 'react-uuid'
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Here is a UUID: {uuid()}
         </p>
         <a
           className="App-link"
